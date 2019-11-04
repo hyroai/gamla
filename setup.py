a86251eb1b01301948a4ff8521d8b5b333b568b9
uri <uri.valevski@gmail.com>
2019-11-04 02:50:54 +0200

    add travis

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gamla",
-    version="0.0.2",
+    version="0.0.3",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
