601ad954fc3686280c5a5db072c6c5ccabf392e0
uri <uri.valevski@gmail.com>
2019-11-15 16:51:05 -0500

    remove gevent dep

--- gamla/io_utils.py
+++ gamla/io_utils.py
@@ -1,12 +1,13 @@
 import functools
 import logging
+import threading
 import time
+from concurrent import futures
 
 import requests
 import requests.adapters
-import requests.packages.urllib3.util.retry
 import toolz
-from gevent import pool
+from requests.packages.urllib3.util import retry
 
 
 def timeit(f):
@@ -25,23 +26,44 @@ def timeit(f):
 
 def requests_with_retry(retries: int = 3) -> requests.Session:
     session = requests.Session()
-    retry = requests.packages.urllib3.util.retry.Retry(
-        total=retries, backoff_factor=0.1, status_forcelist=(500, 502, 504)
+    adapter = requests.adapters.HTTPAdapter(
+        max_retries=retry.Retry(
+            total=retries, backoff_factor=0.1, status_forcelist=(500, 502, 504)
+        )
     )
-    adapter = requests.adapters.HTTPAdapter(max_retries=retry)
     session.mount("http://", adapter)
     session.mount("https://", adapter)
     return session
 
 
-# TODO(uri): Remove in favor of `pmap`.
-def curried_pmap(f):
-    def inner(it):
-        return pool.Group().map(f, it)
+def spawn(f):
+    with futures.ThreadPoolExecutor(max_workers=1) as executor:
+        return executor.submit(f)
 
-    return inner
+
+def spawn_later(s, f):
+    threading.Timer(s, f).start()
 
 
 @toolz.curry
 def pmap(f, it):
-    return pool.Group().map(f, it)
+    return tuple(futures.ThreadPoolExecutor().map(f, it))
+
+
+def make_promise():
+    lock = threading.Semaphore()
+    lock.acquire()
+    resolved_value = None
+
+    def resolve(value):
+        nonlocal resolved_value
+        resolved_value = value
+        lock.release()
+
+    def wait():
+        with lock:
+            if isinstance(resolved_value, Exception):
+                raise resolved_value
+            return resolved_value
+
+    return resolve, wait
