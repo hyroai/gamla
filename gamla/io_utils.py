bebd9529e2a06d4d6406d424d3da5eeaf51c30c7
uri <uri.valevski@gmail.com>
2019-11-17 10:21:27 -0500

    style + remove redundant

--- gamla/io_utils.py
+++ gamla/io_utils.py
@@ -4,9 +4,9 @@ import time
 
 import requests
 import requests.adapters
-import requests.packages.urllib3.util.retry
 import toolz
 from gevent import pool
+from requests.packages.urllib3.util import retry
 
 
 def timeit(f):
@@ -25,21 +25,14 @@ def timeit(f):
 
 def requests_with_retry(retries: int = 3) -> requests.Session:
     session = requests.Session()
-    retry = requests.packages.urllib3.util.retry.Retry(
-        total=retries, backoff_factor=0.1, status_forcelist=(500, 502, 504)
+    adapter = requests.adapters.HTTPAdapter(
+        max_retries=retry.Retry(
+            total=retries, backoff_factor=0.1, status_forcelist=(500, 502, 504)
+        )
     )
-    adapter = requests.adapters.HTTPAdapter(max_retries=retry)
     session.mount("http://", adapter)
     session.mount("https://", adapter)
     return session
-
-
-# TODO(uri): Remove in favor of `pmap`.
-def curried_pmap(f):
-    def inner(it):
-        return pool.Group().map(f, it)
-
-    return inner
 
 
 @toolz.curry
