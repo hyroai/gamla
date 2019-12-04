b2b37673df809bbc4808784f17130f305642ed8f
uri <uri.valevski@gmail.com>
2019-12-04 11:16:02 +0200

    apost

--- gamla/io_utils.py
+++ gamla/io_utils.py
@@ -2,9 +2,16 @@ import functools
 import logging
 import time
 
+import aiohttp
 import requests
 import requests.adapters
 from requests.packages.urllib3.util import retry
+
+
+async def apost(url, json, timeout):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url=url, json=json, timeout=timeout) as response:
+            return await response.json()
 
 
 def timeit(f):
@@ -13,9 +20,9 @@ def timeit(f):
         start = time.time()
         result = f(*args, **kwargs)
         elapsed = time.time() - start
-        logging.info(
-            f"Elapsed time {f.__name__}: {elapsed} ({str(args)[:100]}, {str(kwargs)[:100]})"
-        )
+        args_str = str(args)[:100]
+        kwargs_str = str(kwargs)[:100]
+        logging.info(f"Elapsed time {f.__name__}: {elapsed} ({args_str}, {kwargs_str})")
         return result
 
     return wrapper
