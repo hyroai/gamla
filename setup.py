89b984b23803b41f509acd80b66912f8d358019b
uri <uri.valevski@gmail.com>
2020-02-29 11:44:23 +0200

    increment

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gamla",
-    version="0.0.38",
+    version="0.0.39",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
