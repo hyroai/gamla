d0df13dcc762132d07717e10ca57f8b91177381d
uri <uri.valevski@gmail.com>
2020-03-20 21:00:45 +0200

    `reverse_graph`

--- gamla/graph.py
+++ gamla/graph.py
@@ -64,6 +64,10 @@ graph_to_edges = toolz.compose_left(
     curried.mapcat(functional.star(itertools.product)),
 )
 
+reverse_graph = toolz.compose_left(
+    graph_to_edges, curried.map(toolz.compose_left(reversed, tuple)), edges_to_graph
+)
+
 
 cliques_to_graph = toolz.compose_left(
     curried.mapcat(lambda clique: itertools.permutations(clique, r=2)), edges_to_graph
