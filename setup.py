02a62489408898cbd15fb472d0398cf77e3e6785
david <david@airbud.io>
2020-03-31 11:57:26 +0300

    add dataclasses_json to install_requires in setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gamla",
-    version="0.0.59",
+    version="0.0.60",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
     install_requires=[
         "async-timeout",
+        "dataclasses_json",
         "heapq_max",
         "pytest-asyncio",
         "pytest",
