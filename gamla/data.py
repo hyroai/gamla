cd727aa6398c8dcf68bd65ab8c01e540ef74a580
Nitzan Bar <nitzanbar@gmail.com>
2020-04-02 01:58:19 +0300

    better `freeze_deep`

--- gamla/data.py
+++ gamla/data.py
@@ -18,22 +18,30 @@ def get_encode_config():
     )
 
 
-def freeze_deep(dict_to_freeze: Dict) -> frozendict.frozendict:
-    dict_to_freeze = dict(dict_to_freeze)  # In case input is already a `frozendict`.
-    for key in dict_to_freeze:
-        if isinstance(dict_to_freeze[key], str):
-            continue
-        if isinstance(dict_to_freeze[key], dict):
-            dict_to_freeze[key] = freeze_deep(dict_to_freeze[key])
-        elif isinstance(dict_to_freeze[key], Iterable):
-            dict_to_freeze[key] = toolz.pipe(
-                dict_to_freeze[key],
-                curried.map(
-                    functional.curried_ternary(
-                        lambda x: isinstance(x, dict), freeze_deep, toolz.identity
-                    )
-                ),
-                tuple,
-            )
+def _freeze_deep_inner(value):
+    if isinstance(value, str):
+        return value
+    if isinstance(value, dict):
+        return freeze_deep(value)
+    elif isinstance(value, Iterable):
+        return toolz.pipe(
+            value,
+            curried.map(
+                functional.curried_ternary(
+                    lambda x: isinstance(x, dict), freeze_deep, toolz.identity
+                )
+            ),
+            tuple,
+        )
 
-    return frozendict.frozendict(dict_to_freeze)
+    return value
+
+
+def freeze_deep(dict_to_freeze: Dict) -> frozendict.frozendict:
+    return toolz.pipe(
+        dict_to_freeze,
+        dict, # In case input is already a `frozendict`.
+        curried.valmap(_freeze_deep_inner),
+        frozendict.frozendict
+    )
+
