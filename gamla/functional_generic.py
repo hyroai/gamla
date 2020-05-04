7cfa0ba02ee7f17f439b187f12dbc3a7c06ac900
uri <uri.valevski@gmail.com>
2020-05-05 01:31:55 +0300

    early return

--- gamla/functional_generic.py
+++ gamla/functional_generic.py
@@ -12,5 +12,4 @@ def compose_left(*funcs):
 def compose(*funcs):
     if functional.anymap(asyncio.iscoroutinefunction, funcs):
         return functional_async.acompose(*funcs)
-    else:
-        return toolz.compose(*funcs)
+    return toolz.compose(*funcs)
