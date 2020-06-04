f3611e38abe7a659a6888c70b82e72fd76617a23
david <david@airbud.io>
2020-06-04 13:31:03 +0300

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gamla",
-    version="1.0.3",
+    version="3",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
