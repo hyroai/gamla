375c0778e94c6a1a9a2222eb3c02608622dd2eb8
Nitzan Bar <nitzanbar@gmail.com>
2020-10-15 11:21:33 +0300

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gamla",
-    version="30",
+    version="31",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
