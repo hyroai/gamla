c3e03d1d39bd573fd1f24fa09400dfdb001eb8d4
itayzit <itay@hyro.ai>
2020-12-30 15:38:32 +0200

    anged curry

--- gamla/currying.py
+++ gamla/currying.py
@@ -11,18 +11,16 @@ class _Curry:
 
     def __call__(self, *args, **kwargs):
         try:
-            if asyncio.iscoroutinefunction(self.function):
-                async def curry_async(f):
-                    return await f(*args, **kwargs)
-                return curry_async(self.function)
-
             return self.function(*args, **kwargs)
-        except TypeError:
+        except TypeError as e:
+            print(f"itay {e}")
             self.function = functools.partial(self.function, *args, **kwargs)
             return self
 
 
 def curry(f):
+    if asyncio.iscoroutinefunction(f):
+        return old_curry(f)
     return functools.wraps(f)(_Curry(function=f))
 
 
@@ -62,56 +60,51 @@ def _infer_defaults(f):
     return kwargs
 
 
-# def curry(f):
-#     """
-#     Make a function handle partial input, returning a function that expects the rest.
-#
-#     Warning: uses `inspect` which is slow, so avoid calling this inside a loop.
-#
-#     >>> def addition(a, b): return a + b
-#     >>> add_3 = gamla.curry(addition)(3)
-#     >>> add_3(7)
-#     10
-#
-#     Can also be used as a decorator:
-#     ```
-#     >>>@gamla.curry
-#     ... def addition(a, b):
-#     ...    return a + b
-#     ```
-#
-#     In case the function is async, the function becomes synchronous until the last argument.
-#     Although this is not always what you want, it fits the majority of cases.
-#
-#     ```
-#     >>> @gamla.curry
-#     ... async def async_addition(a, b):
-#     ...    await asyncio.sleep(0.1)
-#     ...    return a + b
-#
-#     >>> add_3 = async_addition(3)
-#
-#     >>> await add_3(7)  # Note that `await` is needed here. Must be done inside an async scope.
-#     ```
-#
-#     The variables can be given with keywords, but mixing keyword and call by order might have unexpected results.
-#     """
-#     f_len_args = inspect.signature(f).parameters
-#     assert (
-#         len(f_len_args) > 1
-#     ), f"Curry function must have at least 2 parameters, {f} has {len(f_len_args)}"
-#     defaults = _infer_defaults(f)
-#     is_coroutine = asyncio.iscoroutinefunction(f)
-#
-#     @functools.wraps(f)
-#     def indirection(*args, **kwargs):
-#         return _curry_helper(is_coroutine, f_len_args, f, (), defaults, *args, **kwargs)
-#
-#     return indirection
+def old_curry(f):
+    f_len_args = inspect.signature(f).parameters
+    assert (
+        len(f_len_args) > 1
+    ), f"Curry function must have at least 2 parameters, {f} has {len(f_len_args)}"
+    defaults = _infer_defaults(f)
+    is_coroutine = asyncio.iscoroutinefunction(f)
 
-async def addition(x, y):
-    return await x + y
+    @functools.wraps(f)
+    def indirection(*args, **kwargs):
+        return _curry_helper(is_coroutine, f_len_args, f, (), defaults, *args, **kwargs)
+
+    return indirection
 
 
-add = curry(addition)
-add5 = add(5)
+def addition(x):
+    return x + 1
+
+
+def compose(f1, f2):
+    def composition(x):
+        return f1(f2(x))
+
+    return composition
+
+
+@curry
+def after(f1, f2):
+    """Second-order composition of `f1` over `f2`."""
+    return compose(f1, f2)
+
+
+b = after(addition)
+
+
+def curried_map(f):
+    return curried_map_sync(f)
+
+
+def curried_map_sync(f):
+    def curried_map(it):
+        for x in it:
+            yield f(x)
+
+    return curried_map
+
+
+map_filter_empty = compose(curried_map, after(curried_map(lambda x: x)))
