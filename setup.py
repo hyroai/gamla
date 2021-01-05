cd09b41f8345c9c6842ee6d885a46c5e54e028ab
Yoni Isaacs <yoniisaacs@Yonis-MacBook-Pro.local>
2021-01-05 20:27:10 +0200

    Version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gamla",
-    version="65",
+    version="66",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
