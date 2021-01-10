1578a088979145703028ddbd6ec331129f910e0c
itayzit <itay@hyro.ai>
2021-01-10 19:51:52 +0200

    add code that generates api.rst automatically

---
+++ docs/source/make_api.py
@@ -0,0 +1,60 @@
+import gamla
+from gamla import *
+
+
+def get_modules():
+    return [
+        currying,
+        data,
+        dict_utils,
+        excepts_decorator,
+        functional,
+        functional_async,
+        functional_generic,
+        graph,
+        graph_async,
+        higher_order,
+        io_utils,
+        string,
+        tree,
+        url_utils,
+    ]
+    # TODO(itay): find a way to get all modules in gamla automatically. this line below can only get filenames
+    # return [importlib.import_module(module) for module in os.listdir('/Users/itayzit/projects/gamla/gamla') if module.endswith('.py') and 'test' not in module]
+
+
+def _get_function_table_entries(module) -> Text:
+    return "".join(
+        [
+            f"   {o[0]}\n"
+            for o in inspect.getmembers(module)
+            if inspect.isfunction(o[1]) and o[0][0] != "_"
+        ],
+    )
+
+
+def _get_module_table_string(string_so_far: Text, module) -> Text:
+    return (
+        string_so_far
+        + f"{module.__name__[6:]}\n{len(module.__name__[6:]) * '-'}\n\n.. currentmodule:: {module.__name__}\n\n.. autosummary::\n{_get_function_table_entries(module)}\n"
+    )
+
+
+def _get_module_members_string(string_so_far: Text, module) -> Text:
+    return string_so_far + f".. automodule:: {module.__name__}\n   :members:\n\n"
+
+
+def create_api_string(modules: Iterable) -> Text:
+    new_api_string = "API\n===\n\n"
+    return gamla.reduce(
+        _get_module_members_string,
+        gamla.reduce(_get_module_table_string, new_api_string, modules)
+        + "Definitions\n-----------\n\n",
+        modules,
+    )
+
+
+print(create_api_string(get_modules()))
+# new_api = open("api.rst", "w")
+# new_api.write(create_api_string(get_modules))
+# new_api.close()
