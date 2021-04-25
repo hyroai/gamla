6b37b912a0b785d82d493821b357d5450452d6c3
Uri <uri.valevski@gmail.com>
2021-04-25 16:58:07 +0300

    release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gamla",
-    version="80",
+    version="81",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
