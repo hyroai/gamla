c2cb09cffc26ec02e95696cf0e96669e8b5cca4a
Uri <uri.valevski@gmail.com>
2021-12-06 16:50:03 +0200

    add `python_requires`

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="gamla",
     version="115",
+    python_requires=">=3.9.5",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
