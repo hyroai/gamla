c0df9e2b1dd0c4f31e643728a8672b79d3894f85
Uri <uri.valevski@gmail.com>
2022-04-14 12:05:03 +0300

    nicer-sort

--- gamla/immutable_set.py
+++ gamla/immutable_set.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Tuple
+from typing import Iterable
 
 import immutables
 
@@ -27,24 +27,15 @@ def contains(set: ImmutableSet, element) -> bool:
     return element in set
 
 
-def _find_larger_smaller(
-    set1: ImmutableSet,
-    set2: ImmutableSet,
-) -> Tuple[ImmutableSet, ImmutableSet]:
-    if len(set1) > len(set2):
-        return set1, set2
-    return set2, set1
-
-
 def union(set1: ImmutableSet, set2: ImmutableSet) -> ImmutableSet:
-    larger, smaller = _find_larger_smaller(set1, set2)
+    smaller, larger = sorted([set1, set2], key=len)
     for element in smaller:
         larger = add(larger, element)
     return larger
 
 
 def intersection(set1: ImmutableSet, set2: ImmutableSet) -> ImmutableSet:
-    larger, smaller = _find_larger_smaller(set1, set2)
+    smaller, larger = sorted([set1, set2], key=len)
     for element in smaller:
         if element not in larger:
             smaller = remove(smaller, element)
