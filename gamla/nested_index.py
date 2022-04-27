89838fb8b418a07f735853ba600e0af07bdb7560
itayzit <itay@hyro.ai>
2022-04-27 17:34:33 +0300

    change name

--- gamla/nested_index.py
+++ gamla/nested_index.py
@@ -3,10 +3,10 @@ from typing import Callable, Dict, Iterable, Tuple
 
 from gamla import functional_generic, operator
 
-HierarchicalIndex = Tuple[dict, int]
+NestedIndex = Tuple[dict, int]
 
-_dict = operator.head
-_steps = operator.second
+_index_dict = operator.head
+_num_of_steps = operator.second
 
 
 def _return_after_n_calls(n: int, value) -> Callable:
@@ -46,14 +46,14 @@ def _make_index_dict(steps):
     )
 
 
-def build(steps: Iterable, it) -> HierarchicalIndex:
+def build(steps: Iterable, it) -> NestedIndex:
     steps = tuple(steps)
     return _make_index_dict(steps)(it), len(steps)
 
 
-def to_query(index: HierarchicalIndex) -> Callable:
+def to_query(index: NestedIndex) -> Callable:
     return _dict_to_getter_with_default_recursive(
         frozenset(),
-        _steps(index),
-        _dict(index),
+        _num_of_steps(index),
+        _index_dict(index),
     )
