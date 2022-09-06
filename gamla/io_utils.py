00a9ad31144cebf7bace8470342c887445cbfb06
eli <eli@hyro.ai>
2022-09-06 15:28:21 +0300

    Change get_async to always follow redirects

--- gamla/io_utils.py
+++ gamla/io_utils.py
@@ -210,7 +210,12 @@ async def get_async_with_headers(headers: Dict[str, str], timeout: float, url: T
     >>> response = await get_async_with_headers({some_header: some_value}, 30, "https://www.someurl.com")
     """
     async with httpx.AsyncClient() as client:
-        return await client.get(url, timeout=timeout, headers=headers)
+        return await client.get(
+            url,
+            timeout=timeout,
+            headers=headers,
+            follow_redirects=True,
+        )
 
 
 #: Performs an async GET request to url with the specified timeout (seconds) and headers.
