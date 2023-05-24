27a252fd12b5ffd3e78cde6d0efb9a67fd919e33
Yoni Isaacs <yoni@hyro.ai>
2023-05-24 14:43:48 +0300

    fix async test

--- gamla/optimized/async_functions_test.py
+++ gamla/optimized/async_functions_test.py
@@ -1,5 +1,5 @@
 from gamla.optimized import async_functions
 
 
-def test_double_star():
-    async_functions.double_star(lambda x: x + 1)({"x": 2}) == 3
+async def test_double_star():
+    await async_functions.double_star(lambda x: x + 1)({"x": 2}) == 3
