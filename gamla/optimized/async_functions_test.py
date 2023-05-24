443380b280cc005ebb9b41374c305f19e96bf878
Yoni Isaacs <yoni@hyro.ai>
2023-05-24 14:58:34 +0300

    fix test

--- gamla/optimized/async_functions_test.py
+++ gamla/optimized/async_functions_test.py
@@ -2,4 +2,7 @@ from gamla.optimized import async_functions
 
 
 async def test_double_star():
-    await async_functions.double_star(lambda x: x + 1)({"x": 2}) == 3
+    async def increment(x):
+        return x + 1
+
+    assert await async_functions.double_star(increment)({"x": 2}) == 3
