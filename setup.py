f1e8f3ea3017c52dd21fc50f137dccae9ddec6ce
amnon-hyro <125553358+amnon-hyro@users.noreply.github.com>
2023-12-28 14:11:22 +0200

    bump release tag to 155

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gamla",
-    version="154",
+    version="155",
     python_requires=">=3.11",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
